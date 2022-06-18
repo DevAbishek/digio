76deae8feeb97a83a2b8bc2cb7dc43d0017bb962
Abishek Agarwal <disk.abishek@gmail.com>
2022-06-18 13:06:35 +0530

    items filter added

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,6 +10,22 @@ const HomePage = () => {
 
     const dispatch = useDispatch();
     const [itemsData, setItemsData] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('drinks')
+    const categories = [
+        {
+            name: 'drink',
+            imageUrl: 'https://cdn-icons-png.flaticon.com/512/4329/4329542.png'
+        },
+        {
+            name: 'rice',
+            imageUrl: 'https://cdn-icons.flaticon.com/png/512/1531/premium/1531334.png?token=exp=1655537840~hmac=129635315c351b9ceaeb730d0bc8cfc8'
+
+        },
+        {
+            name: 'noodles',
+            imageUrl: 'https://cdn-icons.flaticon.com/png/512/3041/premium/3041130.png?token=exp=1655537885~hmac=6d6346e7f346257ca80c5d0528e6f935'
+        }
+    ]
     const url = 'http://localhost:8080/';
 
     useEffect(() => {
@@ -29,11 +45,23 @@ const HomePage = () => {
     return (
         <>
             <DefaultLayout>
+                <div className='d-flex'>
+                    {categories.map(category => (
+                        <div
+                            key={category.name}
+                            className={`category ${selectedCategory === category.name && 'category-active'}`}
+                            onClick={() => setSelectedCategory(category.name)}
+                        >
+                            <h4>{category.name}</h4>
+                            <img src={category.imageUrl} alt={category.name} height="40" width="60" />
+                        </div>
+                    ))}
+                </div>
                 <Row>
                     {
-                        itemsData.map(item => (
+                        itemsData.filter(i => i.category === selectedCategory).map(item => (
                             <Col xs={24} lg={6} md={12} sm={12}>
-                                <ItemList item={item} />
+                                <ItemList key={item.id} item={item} />
                             </Col>
                         ))
                     }
